9b8227590767a7c2c12fc61bc7c168b413b6a935
Ray Myers <raydog87@gmail.com>
2021-04-04 22:56:59 -0700

    Script for taging releases

---
+++ scripts/newVersion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION="$1"
+
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]] ; then
+    >&2 echo "Error: Not a semver: $VERSION"
+    exit 1
+fi
+
+echo "Releasing as: $VERSION"
+
+git commit --allow-empty -m "Release: $VERSION"
+git tag -a "$VERSION" -m "Release $VERSION"
+git push --tags
