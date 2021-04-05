f324cb3025b0f7cb938235c96646ac4ab1fb47e4
Ray Myers <raydog87@gmail.com>
2021-04-04 23:46:44 -0700

    Update release script to update README version

--- scripts/newVersion.sh
+++ scripts/newVersion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PWD="$(dirname "$0")"
 VERSION="$1"
 
 if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]] ; then
@@ -9,6 +10,14 @@ fi
 
 echo "Releasing as: $VERSION"
 
+DENO_LIB_URL="https://deno.land/x/statsd@$VERSION/mod.ts"
+
+# Edit the current README.md library version:
+sed -Ei 's|https://deno.land/x/statsd@(.*?)/mod.ts|'$DENO_LIB_URL'|' "$PWD/../README.md"
+
+# Make a release commit:
 git commit --allow-empty -m "Release: $VERSION"
+
+# Tag and push:
 git tag -a "$VERSION" -m "Release $VERSION"
 git push && git push --tags
